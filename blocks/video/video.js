212b42f86a0eaa1ac6cd540fc346bba324bbf933
Rob Rusher <rrusher@adobe.com>
2024-07-22 12:32:50 -0600

    styling update

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -1,20 +1,21 @@
 import { loadScript } from '../../scripts/scripts.js';
+import { div } from '../../scripts/dom-helpers.js';
 
 export default function decorate(block) {
   const playerID = block.querySelector('div:nth-of-type(1) > div:nth-of-type(2)')?.textContent;
   const entryID = block.querySelector('div:nth-of-type(2) > div:nth-of-type(2)')?.textContent;
+  const pic = block.querySelector('picture');
   [...block.querySelectorAll('div:nth-of-type(-n+2)')].forEach((div) => div.remove());
 
+  const picWrapper = div({ class: 'overlay' }, pic);
   const tagDiv = document.createElement('div');
   tagDiv.classList.add('video-player');
   const tagDivId = `kaltura_player_${playerID}`;
   tagDiv.id = tagDivId;
+  block.append(picWrapper);
   block.append(tagDiv);
 
   let uiConfId = 24639831;
-/*   if (block.classList.contains('keep-same-video-interface-on-mobile')) {
-    uiConfId = 30358591;
-  } */
 
   window.setTimeout(() => {
     loadScript('https://cdnapisec.kaltura.com/p/1759891/sp/175989100/embedIframeJs/uiconf_id/24639831/partner_id/1759891', () => {
